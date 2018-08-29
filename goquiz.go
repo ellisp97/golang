1e436c8cafe483f92854b35d8967a460c44d6110
ellisp97 <ellis97.ep@gmail.com>
2018-08-29 12:13:48 +0100

    simple golang web server / url short dir

--- goquiz.go
+++ goquiz.go
@@ -5,6 +5,8 @@ import (
 	"os"
 	"fmt"
 	"encoding/csv"
+	"strings"
+	"time"
 )
 
 type problem struct {
@@ -14,6 +16,7 @@ type problem struct {
 
 func main() {
 	csvFileName := flag.String("csv", "problems.csv", "CSV File: Question , Answer")
+	timeLimit := flag.Int("limit", 30, "the time limit for the quiz in seconds")
 	flag.Parse()
 
 	file, err := os.Open(*csvFileName)
@@ -26,19 +29,28 @@ func main() {
 		exit("Failed to parse CSV File")
 	}
 	problems := parseLines(lines)
-	fmt.Println(problems)
+
+	timer := time.NewTimer(time.Duration(*timeLimit) * time.Second)
 
 	correct := 0
 	for i,problem := range problems {
-		fmt.Printf("Problem #%d: %s = \n" ,i+1 , problem.q)
-		var answer string
-		fmt.Scanf("%s \n", &answer)
-		if answer == problem.a {
-			correct++
+		fmt.Printf("Problem #%d: %s = " ,i+1 , problem.q)
+		answerChan := make(chan string)
+		go func() {
+			var answer string
+			fmt.Scanf("%s \n", &answer)
+			answerChan <- answer
+		}()
+		select {
+		case <-timer.C:
+			fmt.Printf("\nYou scored %d out %d.\n", correct, len(problems))
+			return
+		case answer := <-answerChan:
+			if answer == problem.a{
+				correct++
+			}
 		}
 	}
-
-	fmt.Printf("You scored %d out %d", correct, len(problems))
 }
 
 
@@ -46,8 +58,8 @@ func parseLines(lines [][]string) []problem {
 	problems := make([]problem, len(lines))
 	for i, line := range lines {
 		problems[i] = problem{
-			q: line[0],
-			a:line[1],
+			q: strings.TrimSpace(line[0]),
+			a: strings.TrimSpace(line[1]),
 		}
 	}
 	return problems
